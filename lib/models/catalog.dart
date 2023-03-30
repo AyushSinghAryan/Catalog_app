282242c205a6346185750c5af9a3f52f25efbb0d
AyushSinghAryan <ayush.singh2019@vitbhopal.ac.in>
2023-03-31 02:20:56 +0530

    Day 10 done

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,15 +1,21 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 class CatalogModel {
-  static final items = [
-    // using static we can directly access the items
-    Item(
-        id: 1,
-        name: "iPhone 14 Pro Max",
-        desc: "Apple iPhone 14th generation",
-        price: 999,
-        color: "#33505a",
-        image:
-            "https://ismart.co.in/wp-content/uploads/2022/09/iphone-14-pro-finish-select-202209-6-1inch-deeppurple.png")
-  ];
+  // catalog.item is also a list of items
+  static List<Item>? items; // real life mey data null rahe ga starting mey
+  // =
+  // [
+  //   // using static we can directly access the items
+  //   Item(
+  //       id: 1,
+  //       name: "iPhone 14 Pro Max",
+  //       desc: "Apple iPhone 14th generation",
+  //       price: 999,
+  //       color: "#33505a",
+  //       image:
+  //           "https://ismart.co.in/wp-content/uploads/2022/09/iphone-14-pro-finish-select-202209-6-1inch-deeppurple.png")
+  // ];
 }
 
 class Item {
@@ -20,5 +26,118 @@ class Item {
   final String? color;
   final String? image;
 
-  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+  Item({
+    this.id,
+    this.name,
+    this.desc,
+    this.price,
+    this.color,
+    this.image,
+  });
+
+  Item copyWith({
+    int? id,
+    String? name,
+    String? desc,
+    num? price,
+    String? color,
+    String? image,
+  }) {
+    return Item(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      desc: desc ?? this.desc,
+      price: price ?? this.price,
+      color: color ?? this.color,
+      image: image ?? this.image,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'desc': desc,
+      'price': price,
+      'color': color,
+      'image': image,
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map['id'] != null ? map['id'] as int : null,
+      name: map['name'] != null ? map['name'] as String : null,
+      desc: map['desc'] != null ? map['desc'] as String : null,
+      price: map['price'] != null ? map['price'] as num : null,
+      color: map['color'] != null ? map['color'] as String : null,
+      image: map['image'] != null ? map['image'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) =>
+      Item.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, desc: $desc, price: $price, color: $color, image: $image)';
+  }
+
+  @override
+  bool operator ==(covariant Item other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.name == name &&
+        other.desc == desc &&
+        other.price == price &&
+        other.color == color &&
+        other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        desc.hashCode ^
+        price.hashCode ^
+        color.hashCode ^
+        image.hashCode;
+  }
 }
+// to help json finding key and values we define a
+// custom constrom with custom name "fromMap" it is decode method
+// key is not string than write dynamic
+// map custom name
+// factory help in chosing a construtor
+// map to class
+// factory Item.fromMap(Map<String, dynamic> map) {
+//   return Item(
+//     id: map["id"],
+//     name: map["name"],
+//     desc: map["desc"],
+//     price: map["price"],
+//     color: map["color"],
+//     image: map["image"],
+//   );
+// }
+
+// comment region other way to genrate it using vs code
+// type data class genrator it will  auto matic genrate it
+
+// encode class to map
+// or we can sey key value ban jaye gi aur value key
+//
+// it matches with backend part
+//   toMap() => {
+//         "id": id,
+//         "name": name,
+//         "desc": desc,
+//         "price": price,
+//         "color": color,
+//         "image": image,
+//       };
+// }
+// if key and value are same than it easy to commuincate
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,30 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_1/models/catalog.dart';
-
+import 'dart:convert';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
 // import 'package:flutter_application_1';
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData(); // custom method
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    // map this decode with map
+    var productsData = decodeData["products"];
+    // we got data in form of list  but we have to map it
+    // we have mltiple data like phone , laptop
+    // list humko miley gi product data sey
+    // .from ka matalab kaha sey list mil rahi hai
+    // now we got list abb hum chatey our list convert into listOfItems
+    // humko map ka type batana hoga
+    // we have data in form of list inside map
+    ///List<Item> list = List.from(productsData)
+    // catalog item bhi  ek list of items hai
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    // agar hummey app restart key rahe gey tho only one data visible
+    // so hum setstate karey gey we  call build mathod again
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     // var name = "Ayush";
-    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
+    // now we dont need dummy list
+    // final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Shopping Assistant"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-            itemCount: dummyList.length,
-            itemBuilder: ((context, index) {
-              return ItemWidget(
-                item: dummyList[index],
-              );
-            })),
+        child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+            ? ListView.builder(
+                // itemCount: dummyList.length,
+                // kyuki inital data null hai so it will give error si no len intial
+                itemCount: CatalogModel.items!.length,
+                itemBuilder: ((context, index) {
+                  return ItemWidget(
+                    // item: dummyList[index],
+                    item: CatalogModel.items![index],
+                  );
+                }))
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
