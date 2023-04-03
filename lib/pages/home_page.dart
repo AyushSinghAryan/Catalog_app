d2930e50a51deec8e403e2d9576cb6d11a237b61
AyushSinghAryan <ayush.singh2019@vitbhopal.ac.in>
2023-04-03 13:15:45 +0530

    day 11 done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:velocity_x/velocity_x.dart';
+
 import 'package:flutter_application_1/models/catalog.dart';
-import 'dart:convert';
+import 'package:flutter_application_1/widgets/theme.dart';
+
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
@@ -47,31 +53,189 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // var name = "Ayush";
-    // now we dont need dummy list
-    // final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Shopping Assistant"),
+      backgroundColor: MyTheme.creamColor,
+      body: SafeArea(
+        child: Container(
+            padding: Vx.m32,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                CatalogHeader(),
+                if (CatalogModel.items != null &&
+                    CatalogModel.items!.isNotEmpty)
+                  CatalogList()
+                      .expand() // means we wrap this widget with expanded widget
+                else
+                  Center(
+                    child: CircularProgressIndicator(),
+                  )
+              ],
+            )),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-            ? ListView.builder(
-                // itemCount: dummyList.length,
-                // kyuki inital data null hai so it will give error si no len intial
-                itemCount: CatalogModel.items!.length,
-                itemBuilder: ((context, index) {
-                  return ItemWidget(
-                    // item: dummyList[index],
-                    item: CatalogModel.items![index],
-                  );
-                }))
-            : Center(
-                child: CircularProgressIndicator(),
-              ),
-      ),
-      drawer: MyDrawer(),
     );
   }
 }
+
+class CatalogHeader extends StatelessWidget {
+  const CatalogHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      "Shopping Assistant".text.xl3.bold.color(MyTheme.darkBluishColor).make(),
+      "Trending products".text.xl.make()
+      // BOth are same OR
+      // Text('Shopping Assistant')
+    ]);
+  }
+}
+
+class CatalogList extends StatelessWidget {
+  const CatalogList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        shrinkWrap: true, //imp
+        itemCount: CatalogModel.items!.length,
+        itemBuilder: ((context, index) {
+          final cataLog = CatalogModel.items![index];
+
+          return CatalogItem(catalog: cataLog);
+        }));
+  }
+}
+
+class CatalogItem extends StatelessWidget {
+  final Item catalog;
+  const CatalogItem({
+    Key? key,
+    required this.catalog,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      children: [
+        CatalogImage(image: catalog.image.toString()),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            catalog.name!.text.lg.color(MyTheme.darkBluishColor).bold.make(),
+            catalog.desc!.text.textStyle(context.captionStyle).make(),
+            10.heightBox,
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceBetween,
+              buttonPadding: EdgeInsets.zero,
+              // here we csn put multiplr buttons
+              children: [
+                "\$${catalog.price}".text.bold.xl.make(),
+                ElevatedButton(
+                    onPressed: (() {}),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(MyTheme.darkBluishColor),
+                        shape: MaterialStateProperty.all(StadiumBorder())),
+                    child: "Buy".text.make())
+              ],
+            ).pOnly(right: 8)
+          ],
+        ))
+      ],
+    )).white.square(150).roundedLg.make().py12();
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+  const CatalogImage({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ;
+    return Image.network(image)
+        .box
+        .rounded
+        .p8
+        .color(MyTheme.creamColor)
+        .make()
+        .p16()
+        .w40(context);
+  }
+}
+
+// image ka stl class
+
+// var name = "Ayush";
+// now we dont need dummy list
+// final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
+// return Scaffold(
+// appBar: AppBar(
+//   title: Text("Shopping Assistant"),
+// ),
+// body: Padding(
+// padding: const EdgeInsets.all(16.0),
+// child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+// now Grid View
+// ? GridView.builder(
+//GridDEligate ek row may kitney items honey yeh define Karey ga
+// mainAxisSpacing use for spacing
+//           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//               crossAxisCount: 2,
+//               mainAxisSpacing: 16,
+//               crossAxisSpacing: 16),
+//           itemBuilder: ((context, index) {
+//             final item = CatalogModel.items![index];
+//             return Card(
+//                 clipBehavior: Clip.antiAlias,
+//                 shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(12)),
+//                 child: GridTile(
+//                   child: Image.network(item.image.toString()),
+//                   header: Container(
+//                     child: Text(
+//                       item.name.toString(),
+//                       style: TextStyle(color: Colors.white),
+//                     ),
+//                     padding: EdgeInsets.all(12),
+//                     decoration: BoxDecoration(color: Colors.deepPurple),
+//                   ),
+//                   footer: Container(
+//                     child: Text(
+//                       item.price.toString(),
+//                       style: TextStyle(color: Colors.white),
+//                     ),
+//                     padding: EdgeInsets.all(12),
+//                     decoration: BoxDecoration(color: Colors.deepPurple),
+//                   ),
+//                 ));
+//           }),
+//           itemCount: CatalogModel.items!.length,
+//         )
+//       // if we use same itemBuilder then problem is wo listtile ki form mey tha
+
+//       // ? ListView.builder(
+//       //     // itemCount: dummyList.length,
+//       //     // kyuki inital data null hai so it will give error si no len intial
+//       //     itemCount: CatalogModel.items!.length,
+//       //     itemBuilder: ((context, index) {
+//       //       return ItemWidget(
+//       //         // item: dummyList[index],
+//       //         item: CatalogModel.items![index],
+//       //       );
+//       //     }))
+//       : Center(
+//           child: CircularProgressIndicator(),
+//         ),
+// ),
+// drawer: MyDrawer(),
--- lib/widgets/theme.dart
+++ lib/widgets/theme.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme {
   ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
+      fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: const AppBarTheme(
           color: Colors.white,
           elevation: 0,
@@ -19,4 +19,8 @@ class MyTheme {
   ThemeData DarkTheme(BuildContext context) => ThemeData(
         brightness: Brightness.dark,
       );
+
+// colors
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xfff403b58);
 }
